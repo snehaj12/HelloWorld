4d05e4ff0a297d8aadc40bec9182e50eaa81e1ac
snehaj12 <89454333+snehaj12@users.noreply.github.com>
2021-08-25 20:10:00 +0530

    Add files via upload
    initial commit

---
+++ src/main/java/demo/mavericks/HelloWorldApplication.java
@@ -0,0 +1,15 @@
+package demo.mavericks;
+
+public class HelloWorldApplication {
+
+	public static void main(String[] args) {
+		System.out.println("HelloWorld application has started!!!");
+
+	}
+	
+	
+	public String getHomePage() {
+		return "Hello World, from Team Mavericks";
+	}
+
+}
